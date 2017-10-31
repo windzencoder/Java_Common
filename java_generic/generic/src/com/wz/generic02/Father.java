d358cea19e4ce61983029a932cc55372859dbd67
wangzhen <869158130@qq.com>
2017-10-31 09:49:43 +0800

    java 枚举

--- java_generic/generic/src/com/wz/generic02/Father.java
+++ java_generic/generic/src/com/wz/generic02/Father.java
@@ -4,6 +4,14 @@ public abstract class Father<T1, T2> {
 	
 }
 
-class C4 extends Father{
+class C1<T1,T2> extends Father<Integer, T2>{}
+
+class C2<T2> extends Father<Integer, T2>{}
+
+class C3 extends Father<Integer, String>{
+	
+}
+
+class C4 extends Father<Object, Object>{
 	
 }
