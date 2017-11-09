24d721cb8ce079d352c3728b039f0f22fc7619c9
wangzhen <869158130@qq.com>
2017-11-09 10:12:11 +0800

    java 克隆

---
+++ java_clone/src/com/wz/test/Department.java
@@ -0,0 +1,37 @@
+package com.wz.test;
+
+public class Department implements Cloneable
+{
+    private int id;
+    private String name;
+ 
+    public Department(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+   
+    
+    
+}
